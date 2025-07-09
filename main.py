f84f119286ca6f5d4a7785dafbf49ef016c1dbc4
g-WSL <gsakore22@gmail.com>
2025-07-09 13:50:21 +0530

    cors allow for 8000

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000",
     "https://space-folio-xi.vercel.app",
     "https://proximacloud.ddns.net"
 ]
